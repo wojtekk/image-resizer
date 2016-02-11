0ec9f4db868fa772274588746ff463af23de31d6
Will Jordan <will@code.org>
2016-02-11 12:02:19 -0800

    add advanced sharp configurations

--- src/streams/optimize.js
+++ src/streams/optimize.js
@@ -26,12 +26,28 @@ module.exports = function () {
 
     if (env.IMAGE_PROGRESSIVE) {
       r.progressive();
+      // An advanced setting for progressive (interlace) JPEG output.
+      // Calculates which spectrum of DCT coefficients uses the fewest bits.
+      // Usually reduces file size at the cost of increased compression time.
+      r.optimiseScans();
     }
 
     // set the output quality
     if (image.modifiers.quality < 100) {
       r.quality(image.modifiers.quality);
     }
+
+    // An advanced setting to reduce the effects of ringing in JPEG output,
+    // in particular where black text appears on a white background (or vice versa).
+    r.overshootDeringing();
+
+    // An advanced setting to apply the use of trellis quantisation with JPEG output.
+    // Reduces file size and slightly increases relative quality at the cost of increased compression time.
+    r.trellisQuantisation();
+
+    // An advanced setting for the zlib compression level of the lossless PNG output format.
+    r.compressionLevel(9);
+
 
     // if a specific output format is specified, set it
     if (image.outputFormat) {
